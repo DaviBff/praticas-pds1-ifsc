bb2035e3d060aeff17f026bbc2da4f8ba3b4e6c5
daviravezeiro <daviboff783@gmail.com>
2022-10-20 21:04:55 -0300

    pratica02

---
+++ pratica02/src/pratica02Ex1/calc.java
@@ -0,0 +1,157 @@
+package pratica02Ex1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.awt.event.ActionEvent;
+
+public class calc extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField N2;
+	private JTextField N1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					calc frame = new calc();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public calc() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 187, 264);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		N2 = new JTextField();
+		N2.setBounds(32, 97, 96, 19);
+		contentPane.add(N2);
+		N2.setColumns(10);
+
+		N1 = new JTextField();
+		N1.setBounds(32, 49, 96, 19);
+		contentPane.add(N1);
+		N1.setColumns(10);
+
+		JLabel lblV1 = new JLabel("Insira o primeiro valor:");
+		lblV1.setBounds(32, 29, 129, 13);
+		contentPane.add(lblV1);
+
+		JLabel lblV2 = new JLabel("Insira o segundo valor:");
+		lblV2.setBounds(32, 74, 141, 13);
+		contentPane.add(lblV2);
+
+		JButton btnSoma = new JButton("+");
+		btnSoma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double num1 = Double.valueOf(N1.getText());
+				Double num2 = Double.valueOf(N2.getText());
+
+				DecimalFormat d = new DecimalFormat("###.##");
+				JOptionPane.showMessageDialog(null, "Soma = " + d.format(somaCalc(num1, num2)));
+			}
+
+			private Double somaCalc(Double num1, Double num2) {
+				Double soma = 0.0;
+				soma = num1 + num2;
+
+				return soma;
+			}
+		});
+		btnSoma.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnSoma.setBounds(37, 141, 46, 33);
+		contentPane.add(btnSoma);
+
+		JButton btnMenos = new JButton("-");
+		btnMenos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double num1 = Double.valueOf(N1.getText());
+				Double num2 = Double.valueOf(N2.getText());
+
+				DecimalFormat d = new DecimalFormat("###.##");
+				JOptionPane.showMessageDialog(null, "Subtracao = " + d.format(subCalc(num1, num2)));
+
+			}
+
+			private Double subCalc(Double num1, Double num2) {
+				Double sub = 0.0;
+				sub = num1 - num2;
+
+				return sub;
+
+			}
+		});
+		btnMenos.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnMenos.setBounds(82, 141, 46, 33);
+		contentPane.add(btnMenos);
+
+		JButton btnDivi = new JButton("/");
+		btnDivi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double num1 = Double.valueOf(N1.getText());
+				Double num2 = Double.valueOf(N2.getText());
+
+				DecimalFormat d = new DecimalFormat("###.##");
+				JOptionPane.showMessageDialog(null, "Divisao = " + d.format(divCalc(num1, num2)));
+
+			}
+
+			private Double divCalc(Double num1, Double num2) {
+				Double div = 0.0;
+				div = num1 / num2;
+
+				return div;
+			}
+		});
+		btnDivi.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnDivi.setBounds(37, 173, 46, 33);
+		contentPane.add(btnDivi);
+
+		JButton btnVezes = new JButton("*");
+		btnVezes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double num1 = Double.valueOf(N1.getText());
+				Double num2 = Double.valueOf(N2.getText());
+
+				DecimalFormat d = new DecimalFormat("###.##");
+				JOptionPane.showMessageDialog(null, "Multiplicacao = " + d.format(multCalc(num1, num2)));
+
+			}
+
+			private Double multCalc(Double num1, Double num2) {
+				Double mult = 0.0;
+				mult = num1 * num2;
+
+				return mult;
+			}
+		});
+		btnVezes.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnVezes.setBounds(82, 173, 46, 33);
+		contentPane.add(btnVezes);
+	}
+}
---
+++ pratica02/src/pratica02Ex2/triangulo.java
@@ -0,0 +1,134 @@
+package pratica02Ex2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+
+public class triangulo extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField Lado1;
+	private JTextField Lado2;
+	private JTextField Lado3;
+	private JTextField Resultado;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					triangulo frame = new triangulo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public triangulo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 372, 291);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		Lado1 = new JTextField();
+		Lado1.setBounds(53, 68, 96, 19);
+		contentPane.add(Lado1);
+		Lado1.setColumns(10);
+
+		Lado2 = new JTextField();
+		Lado2.setBounds(53, 118, 96, 19);
+		contentPane.add(Lado2);
+		Lado2.setColumns(10);
+
+		Lado3 = new JTextField();
+		Lado3.setBounds(53, 172, 96, 19);
+		contentPane.add(Lado3);
+		Lado3.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Insira os lados do triangulo abaixo");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(70, 10, 240, 13);
+		contentPane.add(lblNewLabel);
+
+		JLabel txtl3 = new JLabel("Lado 1:");
+		txtl3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtl3.setBounds(53, 50, 45, 13);
+		contentPane.add(txtl3);
+
+		JLabel txtL2 = new JLabel("Lado 2:");
+		txtL2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtL2.setBounds(53, 97, 45, 13);
+		contentPane.add(txtL2);
+
+		JLabel txtL3 = new JLabel("Lado 3:");
+		txtL3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtL3.setBounds(53, 149, 45, 13);
+		contentPane.add(txtL3);
+
+		JButton btnExibir = new JButton("Exibir");
+		btnExibir.setForeground(new Color(34, 139, 34));
+		btnExibir.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnExibir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Integer L1 = Integer.valueOf(Lado1.getText());
+				Integer L2 = Integer.valueOf(Lado2.getText());
+				Integer L3 = Integer.valueOf(Lado3.getText());
+
+				if (L1 == L2 && L1 == L3) {
+					Resultado.setText(String.valueOf("Equilátero"));
+				} else if (L1 == L2 && L1 != L3) {
+					Resultado.setText(String.valueOf("Isósceles"));
+
+				} else if (L2 == L3 && L2 != L1) {
+					Resultado.setText(String.valueOf("Isósceles"));
+
+				} else if (L1 == L3 && L1 != L2) {
+					Resultado.setText(String.valueOf("Isósceles"));
+
+				} else {
+					Resultado.setText(String.valueOf("Escaleno"));
+
+				}
+
+			}
+		});
+		btnExibir.setBounds(193, 163, 103, 35);
+		contentPane.add(btnExibir);
+
+		Resultado = new JTextField();
+		Resultado.setEditable(false);
+		Resultado.setBounds(200, 118, 96, 19);
+		contentPane.add(Resultado);
+		Resultado.setColumns(10);
+
+		JLabel lblResultado = new JLabel("Resultado:");
+		lblResultado.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblResultado.setBounds(203, 94, 85, 13);
+		contentPane.add(lblResultado);
+
+		JLabel lblNewLabel_1 = new JLabel("=");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(174, 122, 45, 13);
+		contentPane.add(lblNewLabel_1);
+	}
+}
