8b010154143af34ea35dcee2ce9ba3eca0f055c5
davirvezeiro <daviboff783@gmail.com>
2022-10-27 20:51:47 -0300

    rename

--- davi-lindo-git/src/ifsc/MainGit.java
+++ davi-lindo-git/src/ifsc/MainGit.java
@@ -3,8 +3,24 @@ package ifsc;
 public class MainGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
+		/**
+		 * Pega um vetor e multiplicar todos os elementos desse vetor e retorna um novo
+		 * vetor.
+		 * 
+		 * @param vetor
+		 * @return
+		 */
+
+		public Double[] multiplicaVetor(Double[] vetor){
+			Double[] novoVetor = new Double[vetor.length];
+			for (int i = 0; i < vetor.length; i++) {
+				novoVetor[i] = vetor[i] * vetor[i];
+			}
+
+			return novoVetor;
+		}
+		}
 	}
-
-}
